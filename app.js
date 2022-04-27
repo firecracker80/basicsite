c17f39a904d8b59e311b9036844f591264e4b8df
Yajahira Velazquez <yari@yvelazquez.com>
2022-04-27 19:37:28 -0400

    added css and js

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let userName = prompt("What is your name?");
+console.log(userName);
+
+let place = prompt("Welcome " + userName + "! Where have you traveled?");
+console.log(place);
+
+let response = prompt("Awesome! I bet that was fun! Thanks for visiting my site.");
+console.log(response);
+
+document.write(userName);
