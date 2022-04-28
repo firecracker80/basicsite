4c7ef5b69a83f68926bf1dbaa372ad6a00d17c7a
Yajahira Velazquez <yari@yvelazquez.com>
2022-04-28 12:41:49 -0400

    code review session

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ console.log(userName);
 let place = prompt("Welcome " + userName + "! Where have you traveled?");
 console.log(place);
 
-let response = prompt("Awesome! I bet that was fun! Thanks for visiting my site.");
-console.log(response);
+alert("Awesome! I bet that was fun! Thanks for visiting my site.");
 
 document.write("<p id='person-name'>" + userName + "</p>");
