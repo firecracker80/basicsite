29bce4a06f452b53bc010829d28cefe2243b0734
Yajahira Velazquez <yari@yvelazquez.com>
2022-04-28 16:30:39 -0400

    function added

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 'use strict'
 
-let userName = prompt("What is your name?");
-console.log(userName);
+function greeting () {
 
-let place = prompt("Welcome " + userName + "! Where have you traveled?");
+let userName = "";
+let userPrompt = prompt("What is your name?");
+console.log(userName);
+userName += userPrompt
+return userName;
+
+}
+
+// let myUserName = greeting ()
+
+// console.log (myUserName)
+let myUserName = greeting ()
+
+function place () {
+
+// greeting ()
+let place = prompt("Welcome " + myUserName + "! Where have you traveled?");
 console.log(place);
 
-alert("Awesome! I bet that was fun! Thanks for visiting my site.");
+if (place === "Dominican Republic") {
+   let response = prompt ("Where in DR did you go?");
+   console.log (response)
+   alert("Awesome! I bet that was fun! Thanks for visiting my site.");
+}
+else {
+    alert("Awesome! I bet that was fun! Thanks for visiting my site.");
+    }
 
-document.write("<p id='person-name'>" + userName + "</p>");
+// alert("Awesome! I bet that was fun! Thanks for visiting my site.");
+
+document.write("<p id='person-name'>" + myUserName + "</p>");
+
+}
+
+place ()
+
+// let myPlace = place ()
+
+// console.log (myPlace)
