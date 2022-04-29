92fd7f568f280f4a94566f61aef10286000d2279
Yajahira Velazquez <yari@yvelazquez.com>
2022-04-29 14:41:28 -0400

    fixed code

--- app.js
+++ app.js
@@ -10,14 +10,10 @@ return userName;
 
 }
 
-// let myUserName = greeting ()
-
-// console.log (myUserName)
 let myUserName = greeting ()
 
 function country () {
 
-// greeting ();
 let placePrompt = prompt("Welcome " + myUserName + "! Where have you traveled?");
 console.log(placePrompt);
 let place = ""
@@ -26,27 +22,15 @@ if (placePrompt === "dominican republic") {
        
     let response = prompt ("Where in DR did you go?");
    console.log (response.toLowerCase())
-//   /
+
 } else {
     let ask = prompt("How many countries have you visited?");
     console.log (ask);
-    // let number = "";
     if (ask === "3") {
         let pic = ("<img src=\"bernard-hermant-sb2DvfqUtEQ-unsplash.jpg\">");
-        //   return pic * 3
           document.write("<img id='pic' src=bernard-hermant-sb2DvfqUtEQ-unsplash.jpg>");
     }
-    // for (i = 3; i = ask; i++) {
-    //     if (i = ask) {
-        
-        //   let pic = ("<img src=bernard-hermant-sb2DvfqUtEQ-unsplash.jpg>");
-        //   return pic * 3
-        //   document.write("<img id='pic' src=bernard-hermant-sb2DvfqUtEQ-unsplash.jpg>");
-        // }
-    // }
-
-    // document.write("<img id='pic'>" src="bernard-hermant-sb2DvfqUtEQ-unsplash.jpg"</img>");
-    // alert("Awesome! I bet you had fun! Thanks for visiting my site.");
+    
     }
 
     alert("Awesome! I bet you had fun! Thanks for visiting my site.");
@@ -56,7 +40,3 @@ document.write("<p id='person-name'>" + myUserName + "</p>");
 }
 
 country()
-
-// let myPlace = place ()
-
-// console.log (myPlace)
